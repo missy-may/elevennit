0572588a4599717ea0c61b6d84fd03e9a610ef7f
Missy May <missysuemay@gmail.com>
2014-11-11 14:18:23 -0500

    Add styles for forms and flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def flash_class(name)
+    alert_type = case name.to_s
+      when 'notice'  then 'alert-success'
+      when 'error'   then 'alert-danger'
+      when 'alert'   then 'alert-danger'
+      when 'info'    then 'alert-info'
+      when 'warning' then 'alert-warning'
+      else "alert-#{name}"
+    end
+    alert_type
+  end
 end
