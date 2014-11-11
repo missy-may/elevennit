ac111e07a8e0e8138e28437b09c77a7e36645ac7
Missy May <missysuemay@gmail.com>
2014-11-11 10:54:53 -0500

    validate presense and array of flash messages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :title, length: { maximum: 25}
+  validates :title, :link, presence: true
+  validates :title, length: { maximum: 255}, presence: true
 end
