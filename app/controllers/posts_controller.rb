627959f82766e8b95184400dd12352763024c586
Missy May <missysuemay@gmail.com>
2014-11-11 16:04:28 -0500

    Doc 13. partial form. style sheet changes. edit post"

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,27 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find params[:id]
+  end
+
+  def edit
+    @post = Post.find params[:id]
+  end
+
   def new
     @post = Post.new
     @post.post_type = params[:post_type] if params[:post_type].present?
+  end
+
+  def update
+    @post = Post.find params[:id]
+    if @post.update_attributes(params.require(:post).permit(:title, :link, :body, :post_type))
+      redirect_to posts_path, flash: { notice: 'It worked' }
+    else
+      flash.now[:error] = @post.errors.full_messages
+      render :edit
+    end
   end
 
   def create
