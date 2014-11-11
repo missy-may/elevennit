23977eb1abf05b345861432d4c27823f4790b53a
Missy May <missysuemay@gmail.com>
2014-11-11 10:28:58 -0500

    Flash Errors and pry-doc

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,14 +4,19 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  #getting form to update...not the update
   def new
     @post = Post.new
   end
 
   def create
-    post = Post.new(params.require(:post).permit(:title, :link, :body))
-    post.save
-    redirect_to posts_path
+    @post = Post.new(params.require(:post).permit(:title, :link, :body))
+    if @post.save
+      redirect_to posts_path, flash: { notice: 'It Worked.' }
+    else
+      flash.now[:error] = 'Whoops! Something went wrong. Please try again.'
+      render :new
+      # redirect_to new_post_path, flash: { error: 'Whoops! Something went wrong. Please try again.' }
+    end
   end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ActiveRecord::Base
+  validates :title, length: { maximum: 25}
 end
