d1eccfb608b150217448bc5fb12b34a985326de8
Missy May <missysuemay@gmail.com>
2014-11-10 19:15:10 -0500

    Creating a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,14 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  #getting form to update...not the update
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(params.require(:post).permit(:title, :link, :body))
+    post.save
+    redirect_to '/'
+  end
 end
