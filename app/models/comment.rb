8a9d488ff0b8ed358ea3a2896207309d12984797
Missy May <missysuemay@gmail.com>
2014-11-13 13:35:41 -0500

    add sort comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,10 @@
 class Comment < ActiveRecord::Base
   acts_as_nested_set :scope => [:commentable_id, :commentable_type]
-  default_scope { order('created_at ASC') }
+
   validates :body, :presence => true
   validates :user, :presence => true
+
+  default_scope { order('lft ASC, created_at ASC') }
 
   # NOTE: install the acts_as_votable plugin if you
   # want user to vote on the quality of comments.
